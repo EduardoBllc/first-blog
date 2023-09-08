6160d9d933e710ec17b166c6afe66b3bd095b373
Eduardo Ballico <eballico@vendabem.com.br>
2023-09-08 11:03:44 -0300

    adicionando formulário e editor de post

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,45 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)  # Salva o post sem commita-lo
+            post.author = request.user  # Preenche a propriedade author que é obrigatória
+            post.published_date = timezone.now()  # Preenche a propriedade published_date
+            post.save()  # Publica o post
+            return redirect('post_detail', pk=post.pk)  # Redirecionando direto para o post recém criado
+
+    else:
+        form = PostForm
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
